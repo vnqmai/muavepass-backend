1b0ad5d64dda45d36bb7ee292328743f758707d3
maivo <vnqmai01@gmail.com>
2024-11-27 02:57:25 +0700

    update cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-// const cors = require("cors");
+const cors = require("cors");
 const payOS = require("./utils/payos");
 
 require("./db/client");
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 dotenv.config();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -35,6 +35,13 @@ app.post("/create-payment-link", async (req, res) => {
     console.error(error);
     res.send("Something went error");
   }
+});
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 app.listen(PORT, function () {
