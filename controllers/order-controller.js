0d3aa0a823416901c874baba7116c725dbdcc8d2
maivo <vnqmai01@gmail.com>
2024-11-27 05:44:06 +0700

    update send mail to user

--- controllers/order-controller.js
+++ controllers/order-controller.js
@@ -5,6 +5,31 @@ const Product = require("../models/Product");
 const Transaction = require("../models/Transaction");
 const Order = require("../models/Order");
 
+const nodemailer = require('nodemailer');
+
+const transport = {
+  //this is the authentication for sending email.
+host: 'smtp.gmail.com',
+port: 465,
+secure: true, // use TLS
+//create a .env file and define the process.env variables  with your credentials.
+auth: {
+  user: process.env.SMTP_FROM_EMAIL,
+  pass: process.env.SMTP_FROM_PASSWORD,
+},
+}
+
+const transporter = nodemailer.createTransport(transport)
+    transporter.verify((error, success) => {
+if (error) {
+    //if error happened code ends here
+    console.error(error)
+} else {
+    //this means success
+    console.log('Ready to send mail!')
+}
+})
+
 router.post("/create", async function (req, res) {
   const { description, returnUrl, cancelUrl, amount } = req.body;
   const body = {
@@ -43,7 +68,8 @@ router.post("/create", async function (req, res) {
 });
 
 router.post("/create-order-log", async function (req, res) {
-  const { orderCode, productId, userName, userEmail, userPhone, userId } = req.body;
+  const { orderCode, productId, userName, userEmail, userPhone, userId } =
+    req.body;
   const body = {
     order_id: orderCode,
     product_id: productId,
@@ -51,7 +77,7 @@ router.post("/create-order-log", async function (req, res) {
     phone: userPhone,
     fullname: userName,
     status: "IN_PROGRESS",
-    user_id: userId
+    user_id: userId,
   };
 
   Order.create(body).then((order) => {
@@ -65,23 +91,66 @@ router.post("/create-order-log", async function (req, res) {
 
 router.put("/:orderId/success", async function (req, res) {
   const { orderId } = req.params;
-  Order.updateOne({ order_id: orderId.toString() }, { status: "PAID" }).then(() => {
-    return res.json({
-      error: 0,
-      message: "ok",
-      data: null,
-    });
-  })
+  Order.updateOne({ order_id: orderId.toString() }, { status: "PAID" }).then(
+    () => {
+      return res.json({
+        error: 0,
+        message: "ok",
+        data: null,
+      });
+    }
+  );
 });
 
 router.get("/:orderId", async function (req, res) {
   try {
     const order = await payOS.getPaymentLinkInfomation(req.params.orderId);
-    
+
     // await Transaction.insertMany(order.transactions);
+    if (order.status !== "PAID") {
+      return res.json({
+        error: -1,
+        message: "Thanh toán thất bại.",
+        data: null,
+      });
+    }
+
     await Order.updateOne({ order_id: order.orderCode }, { status: "PAID" });
     const orderLog = await Order.findOne({ order_id: order.orderCode });
+
+
     await Product.updateOne({ _id: orderLog.product_id }, { status: "sold" });
+    const product = await Product.findOne({ _id: orderLog.product_id });
+
+    const mail = {
+      from: process.env.SMTP_FROM_EMAIL,
+      to: orderLog.email,
+      subject: `[Muavepass] Thanh toán thành công vé ${product.product_name} - #${order.orderCode}`,
+      text: `
+        Muavepass xin thông báo bạn đã thanh toán thành công vé ${product.product_name} - Vị trí: ${product.seat} - Mã đơn #${order.orderCode}.
+        Vé sẽ được gửi vào mail của bạn trong vòng 1 ngày.
+  
+        Thông tin vé:
+        - Vé： ${product.product_name}
+        - Vị trí: ${product.seat}
+        - Mã đơn: ${order.orderCode}
+        - Giá bán: ${product.price} VND
+  
+        Trân trọng,
+        Muavepass.`,
+    };
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        res.json({
+          status: "fail",
+        });
+      } else {
+        res.json({
+          status: "success",
+        });
+      }
+    });
+    
 
     if (!order || !orderLog) {
       return res.json({
@@ -169,7 +238,9 @@ router.post("/purchase", async (req) => {
     );
 
     if (!ticket) {
-      throw new Error("Vé này đang được chọn và trong quá trình thanh toán. Vui lòng chọn vé khác hoặc thử lại sau ít phút.");
+      throw new Error(
+        "Vé này đang được chọn và trong quá trình thanh toán. Vui lòng chọn vé khác hoặc thử lại sau ít phút."
+      );
     }
     // Commit transaction
     await session.commitTransaction();
